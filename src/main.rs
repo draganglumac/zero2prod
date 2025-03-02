fc0fe0c6844a3ecb481fad127f326917b9adb605
Dragan Glumac <dragan.glumac@gmail.com>
2025-03-02 12:53:49 +0000

    First endpoint - the health check - and its test.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    // Bubble up the io::Error if we failed to bind the address.
+    // Otherwise, call .await on our Server handle.
+    run()?.await
 }

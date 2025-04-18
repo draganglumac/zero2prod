011c67bb61ae2da47afdd845bb49f2d791d50486
Dragan Glumac <dragan.glumac@gmail.com>
2025-04-18 09:15:49 +0100

    Adding ability to connec to DigitalOcean postgres with SSL.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use secrecy::ExposeSecret;
 use sqlx::postgres::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
@@ -13,8 +12,7 @@ async fn main() -> Result<(), std::io::Error> {
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool =
-        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
-            .expect("Failed to create Postgres connection pool.");
+        PgPool::connect_lazy_with(configuration.database.connect_options());
     // We have removed hard-coded `8000` - it's now coming from our settings!
     let address = format!(
         "{}:{}",
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,6 @@
 //! tests/health_check.rs
 
-use secrecy::{ExposeSecret, Secret};
+use secrecy::Secret;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use std::sync::LazyLock;
@@ -36,7 +36,7 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
         ..config.clone()
     };
     let mut connection =
-        PgConnection::connect(&maintenance_settings.connection_string().expose_secret())
+        PgConnection::connect_with(&maintenance_settings.connect_options())
             .await
             .expect("Failed to connect to Postgres");
     connection
@@ -45,7 +45,7 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
         .expect("Failed to create database.");
 
     // Migrate database
-    let connection_pool = PgPool::connect(&config.connection_string().expose_secret())
+    let connection_pool = PgPool::connect_with(config.connect_options())
         .await
         .expect("Failed to connect to Postgres.");
     sqlx::migrate!("./migrations")
